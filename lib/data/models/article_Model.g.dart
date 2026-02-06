3092d4af073e81415778cda1938c6351e2b5c39f
Omar ALSultan <140913566+OmrAl@users.noreply.github.com>
2026-02-06 20:54:49 +0200

    Initial commit

---
+++ lib/data/models/article_Model.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'article_Model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ArticleAdapter extends TypeAdapter<Article> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Article read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Article(
+      title: fields[0] as String,
+      description: fields[1] as String,
+      url: fields[2] as String,
+      urlToImage: fields[3] as String,
+      publishedAt: fields[4] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Article obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.description)
+      ..writeByte(2)
+      ..write(obj.url)
+      ..writeByte(3)
+      ..write(obj.urlToImage)
+      ..writeByte(4)
+      ..write(obj.publishedAt);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ArticleAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
