113f71b3498bb2e35173259d2e7d792c0f20a641
Omar ALSultan <140913566+OmrAl@users.noreply.github.com>
2026-03-22 22:34:33 +0200

    Initial commit

--- lib/presentation/states/base/category_data_when_strategy.dart
+++ lib/presentation/states/base/category_data_when_strategy.dart
@@ -1,13 +1,12 @@
-import '../../../data/models/tab_data.dart';
 import '../../../core/errors/exceptions/app_exception.dart';
 
 
-abstract class CategoryDataWhenStrategy {
+abstract class CategoryDataWhenStrategy<T> {
   R when<R>({
     R Function()? onConnection,
     required R Function() onInitial,
     required R Function() onLoading,
-    required R Function(CategoryData tabData) onLoaded,
+    required R Function(T tabData) onLoaded,
     required R Function(AppException error) onError});
 }
 
